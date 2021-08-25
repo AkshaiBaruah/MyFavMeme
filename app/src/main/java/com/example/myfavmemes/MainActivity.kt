738dacf44acc60b6c0707f9c5216164e50b0d821
Akshai Baruah <akshaib10@gamil.com>
2021-08-25 22:08:46 +0530

    Finalyze and icon added

--- app/src/main/java/com/example/myfavmemes/MainActivity.kt
+++ app/src/main/java/com/example/myfavmemes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfavmemes
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-
-
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -31,9 +30,18 @@ class MainActivity : AppCompatActivity() {
         nextBtn.setOnClickListener {
             loadmeme()
         }
+        shareBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,"Checkout this awesome meme from reddit : $currentImgUrl" )
+            val chooser =Intent.createChooser(intent , "Select an app :")
+            startActivity(chooser)
+        }
 
     }
+    var currentImgUrl :String? = null
 
+    //loadmeme function defined here
     private fun loadmeme(){
         val subredditTxt : TextView = findViewById(R.id.subredditTxt)
         val progressBar : ProgressBar = findViewById(R.id.progressBar)
@@ -41,17 +49,17 @@ class MainActivity : AppCompatActivity() {
         // Instantiate the RequestQueue.
 
         val queue = Volley.newRequestQueue(this)
-        val url = "https://meme-api.herokuapp.com/gimme"
+        val api_link = "https://meme-api.herokuapp.com/gimme"
 
         // Request a jsonObject response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET,
-            url,
+            api_link,
             null,
             { response ->
-                val url = response.getString("url")
+                currentImgUrl = response.getString("url")
                 val subreddit =response.getString("subreddit")
-                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                Glide.with(this).load(currentImgUrl).listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
                         model: Any?,
@@ -76,7 +84,7 @@ class MainActivity : AppCompatActivity() {
                 subredditTxt.text = subreddit
             },
             {
-
+                Toast.makeText(this , "oops try again :(" , Toast.LENGTH_SHORT ).show()
             }
         )
 
